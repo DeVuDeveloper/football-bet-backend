f0b246a48aea8974d1d5fd15d13e2720e2842fe3
VuDej <drvu47@gmail.com>
2022-07-25 12:45:02 +0200

    team and player models and serializers

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_25_100852) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_25_104223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "balance"
+    t.string "starting_balance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "nationality"
+    t.string "full_name"
+    t.string "team_name"
+    t.string "date_of_birth"
+    t.string "date_signed"
+    t.string "birth_location"
+    t.string "contracted_salary"
+    t.string "wage"
+    t.string "description"
+    t.string "position"
+    t.string "height"
+    t.string "weight"
+    t.string "profile_image"
+    t.string "thumbnail"
+    t.bigint "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_players_on_team_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "altName"
+    t.string "founded"
+    t.string "league"
+    t.string "manager"
+    t.string "stadium_location"
+    t.string "home_stadium"
+    t.string "stadium_thumbnail"
+    t.text "stadium_description"
+    t.string "stadium_capacity"
+    t.string "website"
+    t.string "facebook"
+    t.string "twitter"
+    t.string "instagram"
+    t.string "team_logo"
+    t.string "team_jersey"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -29,4 +81,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_25_100852) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "accounts", "users"
+  add_foreign_key "players", "teams"
 end
