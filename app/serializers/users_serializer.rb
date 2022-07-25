e9dc04124e12e1f64e20dc10b035b88f8d001570
VuDej <drvu47@gmail.com>
2022-07-25 12:36:03 +0200

    user and account model and serializers created

---
+++ app/serializers/users_serializer.rb
@@ -0,0 +1,6 @@
+class UsersSerializer
+  include FastJsonapi::ObjectSerializer
+
+  attributes :first_name, :last_name, :phone, :street, :street_2, :city, :state, :country, :zip, :email, :accounts,
+             :bets
+end
