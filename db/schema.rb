01a3eb44c3ffca5c82607348e3a1282146ac5fe9
VuDej <drvu47@gmail.com>
2022-07-26 05:04:17 +0200

    auth controller

--- db/schema.rb
+++ db/schema.rb
@@ -105,6 +105,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_26_023506) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.string "email"
     t.string "password_digest"
     t.string "phone"
     t.string "street"
